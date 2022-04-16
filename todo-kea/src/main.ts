bf22213e8cde1787be4dee6ebe21565717d43945
@HarveyThePooka404 <sylv1612@stud.kea.dk>
2022-04-16 12:26:34 +0200

    removed quasar bits?

--- todo-kea/src/main.ts
+++ todo-kea/src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { Quasar } from 'quasar'
-import quasarUserOptions from './quasar-user-options'
 
 Vue.config.productionTip = false
 
